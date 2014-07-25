096311cb0f5f8d5ee3eb567a7f34d3e792cbacf1
Sameer Sawant <samsaw@gmail.com>
2014-07-25 00:32:55 -0600

    Added method implementation and a test driver method.

--- cachematrix.R
+++ cachematrix.R
@@ -9,7 +9,20 @@
 ## iv)  setting the matrix inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	inverse <- NULL
+	
+	set <- function(y) {
+		x <<- y
+		inverse <<- NULL
+	}
+	
+	get <- function() x
+	
+	setInverse <- function(inv) inverse <<- inv
+	
+	getInverse <- function() inverse
+	
+	list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
 
@@ -20,5 +33,31 @@ makeCacheMatrix <- function(x = matrix()) {
 ## matrix.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    i <- x$getInverse()
+		
+	if(!is.null(i)) {
+		message("Getting cached data.")
+		return (i)
+	}
+
+	data <- x$get()
+	i <- solve(data, ...)
+	x$setInverse(i)
+	i
+}
+
+
+## code to run test on cacheSolve and makeCacheMatrix
+runTest <- function() {
+	x <- matrix(c(2,2,3,2),nr=2,nc=2)
+	v <- makeCacheMatrix(x)
+	print("Input matrix.")
+	print(v$get()) # should print the matrix
+	print("v$getInverse() should be NULL.", quote=FALSE)
+	print(v$getInverse()) # should print NULL
+	cacheSolve(v)
+	print("Diagnostic message 'Getting cached data.' should get printed.", quote=FALSE)
+	cacheSolve(v) # should print "Getting cached data."
+	print("Cached matrix inverse.", quote=FALSE)
+	print(v$getInverse()) # should print the inverse.
 }
